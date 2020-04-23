8a94b7af337c07cd53c8b11e48bf815c7f2a0beb
Mark Brocato <mark.brocato@moovweb.com>
2020-04-23 11:48:16 +0300

    added shopify template

---
+++ shopify/src/routes.ts
@@ -0,0 +1,6 @@
+import { Router } from '@xdn/core/router'
+
+export default new Router()
+  .fallback(({ proxy }) => {
+    proxy('origin')
+  })
---
+++ shopify/xdn.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  routes: "./dist/routes.js"
+  backends: {
+    origin: {
+      domainOrIp: 'www.leifshop.com',
+      hostHeader: 'www.leifshop.com',
+    },
+  },
+}
