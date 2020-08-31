ba900d7f362783dd50ebabca6b9cb8f32423e9b7
kevhender <kevhender@gmail.com>
2020-08-31 10:09:20 -0400

    Add additional files described in Starter docs, with example routes

--- default-starter-template/src/routes.ts
+++ default-starter-template/src/routes.ts
@@ -1,9 +1,21 @@
+import { CACHE_ASSETS } from './cache';
 import { Router } from '@xdn/core/router'
+import shoppingFlowRouteHandler from './shoppingFlowRouteHandler'
 
 export default new Router()
   .get('/service-worker.js', ({ serviceWorker }) => {
     serviceWorker('dist/service-worker.js')
   })
+  // example routes for cacheable pages:
+  .get('/', shoppingFlowRouteHandler)
+  .get('/collections/:path*', shoppingFlowRouteHandler)
+  .get('/products/:path*', shoppingFlowRouteHandler)
+  // example route for cacheable assets:
+  .match('/images/:path*', ({ cache, proxy }) => {
+    cache(CACHE_ASSETS)
+    return proxy('origin')
+  })
+  // fallback route for all other requests:
   .fallback(({ proxy }) => {
     proxy('origin')
   })
